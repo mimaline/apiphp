44c883795be5e3fdc44d4bd37dd5d0f283c8fe2f
gelvazio <gelvazio@tidas.com.br>
2022-11-12 14:05:15 -0300

    ok

---
+++ script.js
@@ -0,0 +1,31 @@
+function abrirMenu01() {
+    document.getElementById("formulario").src = "menus/menu01.html";
+}
+
+function abrirMenu02() {
+    document.getElementById("formulario").src = "menus/menu02.html";
+}
+
+function abrirMenu03() {
+    document.getElementById("formulario").src = "menus/menu03.html";
+}
+
+function abrirMenu04() {
+    document.getElementById("formulario").src = "";
+}
+
+function abrirMenu05() {
+    document.getElementById("formulario").src = "";
+}
+
+function abrirMenu06() {
+    document.getElementById("formulario").src = "";
+}
+
+function abrirIntro() {
+    document.getElementById("retornar").src = "menus/intro.html";
+}
+
+function abrirMenuPessoa() {
+    document.getElementById("formulario").src = "menus/pessoa.html";
+}
